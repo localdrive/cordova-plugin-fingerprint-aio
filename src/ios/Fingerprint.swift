23f8021497a93062f4e62f3c5fc40c72826474ee
robertolocal <roberto@localdrive.com.br>
2020-01-06 15:40:14 -0300

    Update Fingerprint.swift

--- src/ios/Fingerprint.swift
+++ src/ios/Fingerprint.swift
@@ -53,23 +53,9 @@ import LocalAuthentication
         var policy:LAPolicy = .deviceOwnerAuthentication;
         let data  = command.arguments[0] as AnyObject?;
 
-        if let disableBackup = data?["disableBackup"] as! Bool? {
-            if disableBackup {
-                authenticationContext.localizedFallbackTitle = "";
-                policy = .deviceOwnerAuthenticationWithBiometrics;
-            } else {
-                if let localizedFallbackTitle = data?["localizedFallbackTitle"] as! String? {
-                    authenticationContext.localizedFallbackTitle = localizedFallbackTitle;
-                }
-            }
-        }
-
-        // Localized reason
-        if let localizedReason = data?["localizedReason"] as! String? {
-            reason = localizedReason;
-        }else if let clientId = data?["clientId"] as! String? {
-            reason = clientId;
-        }
+        authenticationContext.localizedFallbackTitle = "";
+        policy = .deviceOwnerAuthenticationWithBiometrics;
+        reason = "Autenticação";
 
         authenticationContext.evaluatePolicy(
             policy,
